a4c9fdfd606905ba797db261e1895c48af782a01
Bartosz Rudzki (ROG FLOW) <bartosz.rudzki@tensquaregames.com>
2024-05-13 07:41:55 +0200

    app shadow jar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
+    id("com.github.johnrengelman.shadow") version "7.1.2"
     id("application")
 }
 
@@ -10,4 +11,11 @@ dependencies {
 
 application {
     mainClass = "pl.bratek20.hla.app.Main"
+}
+
+tasks.shadowJar {
+    archiveClassifier.set("") // Remove any classifier to replace the default JAR
+    manifest {
+        attributes("Main-Class" to "pl.bratek20.hla.app.Main")
+    }
 }
