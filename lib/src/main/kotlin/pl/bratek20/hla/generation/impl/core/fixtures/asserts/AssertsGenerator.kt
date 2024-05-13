aea9f292b589285a173d42f0c576200b63975c90
Bartosz Rudzki (ROG FLOW) <bartosz.rudzki@tensquaregames.com>
2024-05-13 19:18:48 +0200

    custom types working for kotlin

--- lib/src/main/kotlin/pl/bratek20/hla/generation/impl/core/fixtures/asserts/AssertsGenerator.kt
+++ lib/src/main/kotlin/pl/bratek20/hla/generation/impl/core/fixtures/asserts/AssertsGenerator.kt
@@ -1,11 +1,14 @@
 package pl.bratek20.hla.generation.impl.core.fixtures.asserts
 
+import pl.bratek20.hla.definitions.api.ComplexStructureDefinition
+import pl.bratek20.hla.definitions.api.TypeDefinition
 import pl.bratek20.hla.directory.api.File
 import pl.bratek20.hla.generation.impl.core.api.ViewType
-import pl.bratek20.hla.generation.impl.core.api.ViewTypeFactory
 import pl.bratek20.hla.generation.impl.core.ModulePartFileGenerator
 import pl.bratek20.hla.generation.impl.core.ModuleGenerationContext
-import pl.bratek20.hla.generation.impl.core.domain.DomainTypeFactory
+import pl.bratek20.hla.generation.impl.core.language.LanguageTypes
+import pl.bratek20.hla.generation.impl.core.web.dto.DtoViewType
+import pl.bratek20.hla.generation.impl.core.web.dto.DtoViewTypeFactory
 
 data class AssertFieldView(
     val name: String,
@@ -16,20 +19,41 @@ data class AssertView(
     val funName: String,
     val givenName: String,
     val expectedName: String,
-    val fields: List<AssertFieldView>
-)
+    val fields: List<AssertFieldView>,
+    val type: ExpectedViewType,
+    val languageTypes: LanguageTypes
+) {
+    //TODO fix copy paste from dto
+    fun getter(variableName: String, field: AssertFieldView): String {
+        if (type is ComplexCustomExpectedViewType) {
+            val x = languageTypes.customTypeGetterName(type.name, field.name)
+            return field.expectedType.assignment("$x($variableName)")
+        }
+        return field.expectedType.assignment("$variableName.${field.name}")
+    }
+}
 
 class AssertsGenerator(
     c: ModuleGenerationContext,
     private val expectedTypeFactory: ExpectedTypeFactory = ExpectedTypeFactory(c.language.types(), c.language.assertsFixture())
 ): ModulePartFileGenerator(c) {
 
+    //TODO fix copy paste from dto
+    private fun myViewType(def: ComplexStructureDefinition): ViewType {
+        val type = TypeDefinition(def.name, emptyList())
+        return viewType(type)
+    }
+
+    private fun myExpectedViewType(def: ComplexStructureDefinition): ExpectedViewType {
+        return expectedTypeFactory.create(myViewType(def))
+    }
+
     private fun expectedType(type: ViewType): ExpectedViewType {
         return expectedTypeFactory.create(type)
     }
 
     override fun generateFile(): File {
-        val asserts = module.complexValueObjects.map {
+        val asserts = (module.complexValueObjects + module.complexCustomTypes).map {
             AssertView(
                 funName =  language.assertsFixture().assertFunName(it.name),
                 givenName = it.name,
@@ -39,7 +63,9 @@ class AssertsGenerator(
                         name = it.name,
                         expectedType = expectedType(viewType(it.type))
                     )
-                }
+                },
+                type = myExpectedViewType(it),
+                languageTypes = language.types()
             )
         }
 
