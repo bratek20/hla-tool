1cad1a5cefdf71761b9f1e403355b80cba0bd335
Bartosz Rudzki (ROG FLOW) <bartosz.rudzki@tensquaregames.com>
2024-05-21 14:02:23 +0200

    deploy

--- scripts/deployWoH.sh
+++ scripts/deployWoH.sh
@@ -2,8 +2,9 @@
 
 wohHlaPath="/mnt/d/Rortos/hla/tool.jar"
 wohPfPath="/mnt/d/Rortos/aircombatcs/hla/tool.jar"
+wohCsPath="/mnt/d/Rortos/content-service/hla/tool.jar"
 
 cp ../app/build/libs/app.jar $wohHlaPath
 cp ../app/build/libs/app.jar $wohPfPath
-
+cp ../app/build/libs/app.jar $wohCsPath
 
