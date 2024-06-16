092365be7e3afd375d1178ab449511617006c82d
Bartosz Rudzki (ROG FLOW) <bartosz.rudzki@tensquaregames.com>
2024-06-16 08:41:38 +0200

    tweak

---
+++ bash/deployStarter.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+starter="/mnt/d/Private/bratek20-starter/bash/tool.jar"
+
+cp ../app/build/libs/app.jar $starter
+
--- bash/deployWoH.sh
+++ bash/deployWoH.sh
@@ -3,8 +3,10 @@
 wohHlaPath="/mnt/d/Rortos/hla/tool.jar"
 wohPfPath="/mnt/d/Rortos/aircombatcs/hla/tool.jar"
 wohCsPath="/mnt/d/Rortos/content-service/hla/tool.jar"
+wohWmPath="/mnt/d/Rortos/ac-common/hla/tool.jar"
 
 cp ../app/build/libs/app.jar $wohHlaPath
 cp ../app/build/libs/app.jar $wohPfPath
 cp ../app/build/libs/app.jar $wohCsPath
+cp ../app/build/libs/app.jar $wohWmPath
 
