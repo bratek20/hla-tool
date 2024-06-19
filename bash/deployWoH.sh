b094cb0c0ba60a2f041174ec61e8d6c92fd5bf57
Bartosz Rudzki (ROG FLOW) <bartosz.rudzki@tensquaregames.com>
2024-06-19 09:30:11 +0200

    deploy script tweak

--- bash/deployWoH.sh
+++ bash/deployWoH.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-wohHlaPath="/mnt/d/Rortos/hla/tool.jar"
-wohPfPath="/mnt/d/Rortos/aircombatcs/hla/tool.jar"
-wohCsPath="/mnt/d/Rortos/content-service/hla/tool.jar"
-wohWmPath="/mnt/d/Rortos/ac-common/hla/tool.jar"
+# Define an array of paths
+paths=(
+    "/mnt/d/Rortos/hla/tool.jar"
+    "/mnt/d/Rortos/aircombatcs/hla/tool.jar"
+    "/mnt/d/Rortos/content-service/hla/tool.jar"
+    "/mnt/d/Rortos/ac-common/hla/tool.jar"
+    "/mnt/d/Rortos/gamelift-play-server/hla/tool.jar"
+)
 
-cp ../app/build/libs/app.jar $wohHlaPath
-cp ../app/build/libs/app.jar $wohPfPath
-cp ../app/build/libs/app.jar $wohCsPath
-cp ../app/build/libs/app.jar $wohWmPath
+# Source path of the app.jar file
+source_path="../app/build/libs/app.jar"
 
+# Copy app.jar to each path in the array
+for path in "${paths[@]}"; do
+    cp "$source_path" "$path"
+    echo "Copied $source_path to $path"
+done
