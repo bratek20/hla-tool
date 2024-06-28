a997eb3da9a2a6c8c0adf05fc771d3fbb69bbb04
Bartosz Rudzki (ROG FLOW) <bartosz.rudzki@tensquaregames.com>
2024-06-28 18:12:36 +0200

    gen

--- lib/src/main/kotlin/com/github/bratek20/hla/parsing/api/Interfaces.kt
+++ lib/src/main/kotlin/com/github/bratek20/hla/parsing/api/Interfaces.kt
@@ -4,6 +4,7 @@ package com.github.bratek20.hla.parsing.api
 
 import com.github.bratek20.hla.definitions.api.*
 import com.github.bratek20.hla.directory.api.*
+import com.github.bratek20.hla.facade.api.*
 
 interface ModuleDefinitionsParser {
     @Throws(
