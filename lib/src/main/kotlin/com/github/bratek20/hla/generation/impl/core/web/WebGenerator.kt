ae64e493ff1eaf723f890dccf17d3298224ed4a7
Bartosz Rudzki (ROG FLOW) <bartosz.rudzki@tensquaregames.com>
2024-06-29 22:29:00 +0200

    step

--- lib/src/main/kotlin/com/github/bratek20/hla/generation/impl/core/web/WebGenerator.kt
+++ lib/src/main/kotlin/com/github/bratek20/hla/generation/impl/core/web/WebGenerator.kt
@@ -1,7 +1,42 @@
 package com.github.bratek20.hla.generation.impl.core.web
 
+import com.github.bratek20.hla.directory.api.FileContent
 import com.github.bratek20.hla.generation.impl.core.DirectoryGenerator
 import com.github.bratek20.hla.generation.impl.core.FileGenerator
+import com.github.bratek20.hla.generation.impl.core.GeneratorMode
+
+class WebCommonGenerator: FileGenerator() {
+    override fun name(): String {
+        return "WebCommon"
+    }
+
+    override fun generateFileContent(): FileContent? {
+        return contentBuilder("webCommon.vm")
+            .build()
+    }
+}
+
+class WebClientGenerator: FileGenerator() {
+    override fun name(): String {
+        return "WebClient"
+    }
+
+    override fun generateFileContent(): FileContent? {
+        return contentBuilder("webClient.vm")
+            .build()
+    }
+}
+
+class WebServerGenerator: FileGenerator() {
+    override fun name(): String {
+        return "WebServer"
+    }
+
+    override fun generateFileContent(): FileContent? {
+        return contentBuilder("webServer.vm")
+            .build()
+    }
+}
 
 class WebGenerator: DirectoryGenerator() {
     override fun name(): String {
@@ -13,11 +48,14 @@ class WebGenerator: DirectoryGenerator() {
     }
 
     override fun shouldGenerateDirectory(): Boolean {
-        return false
+        return c.module.getWebSubmodule() != null
     }
 
     override fun getFileGenerators(): List<FileGenerator> {
         return listOf(
+            WebCommonGenerator(),
+            WebClientGenerator(),
+            WebServerGenerator()
         )
     }
 }
