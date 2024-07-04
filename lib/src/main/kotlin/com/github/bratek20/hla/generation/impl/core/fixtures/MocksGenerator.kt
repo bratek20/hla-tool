6664349bf95fab95595a2baa4c5afe667cc4f36d
Bartosz Rudzki (ROG FLOW) <bartosz.rudzki@tensquaregames.com>
2024-07-04 04:30:08 +0200

    migration started

--- lib/src/main/kotlin/com/github/bratek20/hla/generation/impl/core/fixtures/MocksGenerator.kt
+++ lib/src/main/kotlin/com/github/bratek20/hla/generation/impl/core/fixtures/MocksGenerator.kt
@@ -8,12 +8,23 @@ class MocksGenerator: FileGenerator() {
         return "Mocks"
     }
 
+    class View {
+        fun block(): String {
+            val indent = "    "
+            val result = "// referenceOtherClass\n" +
+            indent + "private var referenceOtherClassCalls = mutableListOf<OtherClass>()\n" +
+            indent + "private val referenceOtherClassResponses = mutableListOf<Pair<ExpectedOtherClass.() -> Unit, OtherClassDef.() -> Unit>>()"
+
+            return result
+        }
+    }
     override fun generateFileContent(): FileContent? {
         if(c.module.getInterfaces().none { it.getName() == "SomeInterface2" }) {
             return null
         }
         return contentBuilder("mocks.vm")
             .put("interfaceName", "SomeInterface2")
+            .put("view", View())
             .build()
     }
 }
