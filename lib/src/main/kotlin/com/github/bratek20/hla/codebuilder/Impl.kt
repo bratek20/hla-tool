6e6314fb05457c0425f8d071d451d9689460c2f9
Bartosz Rudzki (ROG FLOW) <bartosz.rudzki@tensquaregames.com>
2024-07-04 04:52:20 +0200

    code builder module

---
+++ lib/src/main/kotlin/com/github/bratek20/hla/codebuilder/Impl.kt
@@ -0,0 +1,55 @@
+package com.github.bratek20.hla.codebuilder
+
+
+interface CodeLineBuilder {
+    fun build(): String
+}
+
+class ListFieldDeclaration(
+    private val fieldName: String,
+    private val fieldElementType: String
+): CodeLineBuilder {
+    override fun build(): String {
+        return "private val $fieldName = mutableListOf<$fieldElementType>()"
+    }
+}
+
+class ClassDeclaration(
+    private val className: String,
+    private val implementedInterfaceName: String
+): CodeLineBuilder {
+    override fun build(): String {
+        return "class $className: $implementedInterfaceName {"
+    }
+
+}
+class CodeBuilder(
+    indent: Int = 0
+) {
+    private var currentIndent = indent
+    private val lines = mutableListOf<String>()
+
+    fun line(value: String): CodeBuilder {
+        val indent = " ".repeat(currentIndent)
+        lines.add(indent + value)
+        return this
+    }
+
+    fun line(value: CodeLineBuilder): CodeBuilder {
+        return line(value.build())
+    }
+
+    fun tab(): CodeBuilder {
+        currentIndent += 4
+        return this
+    }
+
+    fun untab(): CodeBuilder {
+        currentIndent -= 4
+        return this
+    }
+
+    fun build(): String {
+        return lines.joinToString("\n")
+    }
+}
--- lib/src/main/kotlin/com/github/bratek20/hla/generation/impl/core/fixtures/MocksGenerator.kt
+++ lib/src/main/kotlin/com/github/bratek20/hla/generation/impl/core/fixtures/MocksGenerator.kt
@@ -1,5 +1,8 @@
 package com.github.bratek20.hla.generation.impl.core.fixtures
 
+import com.github.bratek20.hla.codebuilder.ClassDeclaration
+import com.github.bratek20.hla.codebuilder.CodeBuilder
+import com.github.bratek20.hla.codebuilder.ListFieldDeclaration
 import com.github.bratek20.hla.directory.api.FileContent
 import com.github.bratek20.hla.generation.impl.core.FileGenerator
 
@@ -8,58 +11,6 @@ class MocksGenerator: FileGenerator() {
         return "Mocks"
     }
 
-    interface CodeLineBuilder {
-        fun build(): String
-    }
-
-    class ListFieldDeclaration(
-        private val fieldName: String,
-        private val fieldElementType: String
-    ): CodeLineBuilder {
-        override fun build(): String {
-            return "private val $fieldName = mutableListOf<$fieldElementType>()"
-        }
-    }
-
-    class ClassDeclaration(
-        private val className: String,
-        private val implementedInterfaceName: String
-    ): CodeLineBuilder {
-        override fun build(): String {
-            return "class $className: $implementedInterfaceName {"
-        }
-
-    }
-    class CodeBuilder(
-        indent: Int = 0
-    ) {
-        private var currentIndent = indent
-        private val lines = mutableListOf<String>()
-
-        fun line(value: String): CodeBuilder {
-            val indent = " ".repeat(currentIndent)
-            lines.add(indent + value)
-            return this
-        }
-
-        fun line(value: CodeLineBuilder): CodeBuilder {
-            return line(value.build())
-        }
-
-        fun tab(): CodeBuilder {
-            currentIndent += 4
-            return this
-        }
-
-        fun untab(): CodeBuilder {
-            currentIndent -= 4
-            return this
-        }
-
-        fun build(): String {
-            return lines.joinToString("\n")
-        }
-    }
     class View(
         val interfaceName: String
     ) {
