30ba3389c24b9d41f063495f1cc5087a65201599
Bartosz Rudzki (ROG FLOW) <bartosz.rudzki@tensquaregames.com>
2024-07-12 19:49:02 +0200

    green

--- lib/src/main/kotlin/com/github/bratek20/hla/generation/impl/core/fixtures/DiffsGenerator.kt
+++ lib/src/main/kotlin/com/github/bratek20/hla/generation/impl/core/fixtures/DiffsGenerator.kt
@@ -1,7 +1,12 @@
 package com.github.bratek20.hla.generation.impl.core.fixtures
 
+import com.github.bratek20.hla.codebuilder.*
+import com.github.bratek20.hla.codebuilder.Function
+import com.github.bratek20.hla.definitions.api.TypeDefinition
 import com.github.bratek20.hla.directory.api.FileContent
 import com.github.bratek20.hla.generation.impl.core.FileGenerator
+import com.github.bratek20.hla.generation.impl.core.api.ExternalApiType
+import com.github.bratek20.hla.utils.pascalToCamelCase
 
 class DiffsGenerator: FileGenerator() {
     override fun name(): String {
@@ -12,7 +17,8 @@ class DiffsGenerator: FileGenerator() {
         val simpleAssertTypes = modules.allSimpleStructureDefinitions(module)
         val enumTypes = modules.allEnumTypeDefinitions(module)
         val complexAssertTypes = modules.allComplexStructureDefinitions(module)
-        if (simpleAssertTypes.isEmpty() && complexAssertTypes.isEmpty() && enumTypes.isEmpty()) {
+        val externalTypes = modules.allExternalTypesDefinitions(module)
+        if (simpleAssertTypes.isEmpty() && complexAssertTypes.isEmpty() && enumTypes.isEmpty() && externalTypes.isEmpty()) {
             return null
         }
 
@@ -26,9 +32,32 @@ class DiffsGenerator: FileGenerator() {
             factory.create(apiTypeFactory.create(it))
         }
 
+        val externalTypesDiffs = if (externalTypes.isEmpty())
+            null
+        else
+            CodeBuilder()
+                .add(ManyCodeBlocksSeparatedByLine(
+                    externalTypes.map { externalTypeDiff(it) }
+                ))
+                .build()
+
         return contentBuilder("diffs.vm")
             .put("simpleAsserts", simpleAsserts)
             .put("complexAsserts", complexAsserts)
+            .put("externalTypesDiffs", externalTypesDiffs)
             .build()
+    }
+
+    private fun externalTypeDiff(type: TypeDefinition): CodeBlockBuilder {
+        val apiType = apiTypeFactory.create(type) as ExternalApiType
+        return Function(
+            name = "diff" + apiType.rawName,
+            args = listOf("given" to apiType.name(), "expected" to apiType.name(), "path" to "String = \"\""),
+            returnType = "String",
+            body = block {
+                line("if (given != expected) { return \"\${path}value \${given} != \${expected}\" }")
+                line("return \"\"")
+            }
+        )
     }
 }
