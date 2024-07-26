83893dfba98bf97347d00eede5247bb0f1dd57ee
Bartosz Rudzki (ROG FLOW) <bartosz.rudzki@tensquaregames.com>
2024-07-26 13:15:36 +0200

    ts gen

--- example/typescript/Tests/fixtures/SomeModule/Fixtures/Asserts.ts
+++ example/typescript/Tests/fixtures/SomeModule/Fixtures/Asserts.ts
@@ -46,6 +46,11 @@ namespace SomeModule.Assert {
         AssertEquals(diff, "", diff)
     }
 
+    export function classUsingExternalType(given: ClassUsingExternalType, expected: ExpectedClassUsingExternalType) {
+        const diff = diffClassUsingExternalType(given, expected)
+        AssertEquals(diff, "", diff)
+    }
+
     export function recordClass(given: RecordClass, expected: ExpectedRecordClass) {
         const diff = diffRecordClass(given, expected)
         AssertEquals(diff, "", diff)
