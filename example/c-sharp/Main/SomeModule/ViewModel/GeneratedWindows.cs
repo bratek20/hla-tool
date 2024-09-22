911400ec746da429b2e866bfaa13f9a7a984dbca
Bartosz Rudzki (ROG FLOW) <bartosz.rudzki@tensquaregames.com>
2024-09-22 18:42:57 +0200

    green

--- example/c-sharp/Main/SomeModule/ViewModel/GeneratedWindows.cs
+++ example/c-sharp/Main/SomeModule/ViewModel/GeneratedWindows.cs
@@ -3,19 +3,19 @@
 using System;
 using System.Collections.Generic;
 using B20.Ext;
-using B20.Frontend.Traits;
+using B20.Frontend.Windows.Api;
 using B20.Frontend.UiElements;
 using SomeModule.Api;
 using OtherModule.Api;
 using TypesModule.Api;
 
 namespace SomeModule.ViewModel {
-    public class SomeWindowState
-    {
+    public class SomeWindowState {
         public SomeId SomeId { get; }
 
-        public SomeWindowState(SomeId someId)
-        {
+        public SomeWindowState(
+            SomeId someId
+        ) {
             SomeId = someId;
         }
     }
--- lib/src/main/kotlin/com/github/bratek20/hla/generation/impl/core/viewmodel/GeneratedWindowsGenerator.kt
+++ lib/src/main/kotlin/com/github/bratek20/hla/generation/impl/core/viewmodel/GeneratedWindowsGenerator.kt
@@ -1,8 +1,39 @@
 package com.github.bratek20.hla.generation.impl.core.viewmodel
 
+import com.github.bratek20.codebuilder.builders.ClassBuilderOps
+import com.github.bratek20.codebuilder.builders.TopLevelCodeBuilderOps
+import com.github.bratek20.codebuilder.types.typeName
 import com.github.bratek20.hla.generation.api.PatternName
 import com.github.bratek20.hla.generation.impl.core.PatternGenerator
 
+class GeneratedWindowLogic() {
+    fun getState(): ClassBuilderOps = {
+        name = "SomeWindowState"
+
+        addField {
+            type = typeName("SomeId")
+            name = "someId"
+            getter = true
+            fromConstructor = true
+        }
+    }
+
+    fun getWindowClass(): ClassBuilderOps = {
+        name = "SomeWindow"
+        partial = true
+        extends {
+            className = "Window"
+            generic = typeName("SomeWindowState")
+        }
+
+        addField {
+            type = typeName("SomeClassVm")
+            name = "someClassVm"
+            getter = true
+            setter = true
+        }
+    }
+}
 class GeneratedWindowsGenerator: PatternGenerator() {
     override fun patternName(): PatternName {
         return PatternName.GeneratedWindows
@@ -16,5 +47,20 @@ class GeneratedWindowsGenerator: PatternGenerator() {
 
     override fun shouldGenerate(): Boolean {
         return viewModelWindows()?.isNotEmpty() ?: false
+    }
+
+    override fun getOperations(): TopLevelCodeBuilderOps = {
+        viewModelWindows()?.forEach { window ->
+            val logic = GeneratedWindowLogic()
+            addClass(logic.getState())
+            addClass(logic.getWindowClass())
+        }
+    }
+
+    override fun extraCSharpUsings(): List<String> {
+        return listOf(
+            "B20.Frontend.Windows.Api",
+            "B20.Frontend.UiElements"
+        )
     }
 }
