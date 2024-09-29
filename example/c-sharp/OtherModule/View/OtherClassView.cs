4821c951863e7e5bec5518edf2ca09771873f85b
Bartosz Rudzki (ROG FLOW) <bartosz.rudzki@tensquaregames.com>
2024-09-29 20:12:33 +0200

    use api for view

--- example/c-sharp/OtherModule/View/OtherClassView.cs
+++ example/c-sharp/OtherModule/View/OtherClassView.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using B20.Ext;
 using B20.Frontend.Elements.View;
 using UnityEngine;
+using OtherModule.Api;
 using OtherModule.ViewModel;
 
 namespace OtherModule.View {
