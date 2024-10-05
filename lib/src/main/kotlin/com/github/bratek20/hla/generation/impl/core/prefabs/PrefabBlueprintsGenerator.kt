894b97303c741db424c5e1d5d29cf7b95e05b6a2
Bartosz Rudzki (ROG FLOW) <bartosz.rudzki@tensquaregames.com>
2024-10-05 09:38:16 +0200

    step - serialization module formatting support needed

--- lib/src/main/kotlin/com/github/bratek20/hla/generation/impl/core/prefabs/PrefabBlueprintsGenerator.kt
+++ lib/src/main/kotlin/com/github/bratek20/hla/generation/impl/core/prefabs/PrefabBlueprintsGenerator.kt
@@ -1,7 +1,11 @@
 package com.github.bratek20.hla.generation.impl.core.prefabs
 
+import com.github.bratek20.architecture.serialization.context.SerializationFactory
 import com.github.bratek20.hla.generation.api.PatternName
 import com.github.bratek20.hla.generation.impl.core.PatternGenerator
+import com.github.bratek20.hla.prefabcreator.api.BlueprintType
+import com.github.bratek20.hla.prefabcreator.api.PrefabBlueprint
+import com.github.bratek20.hla.prefabcreator.api.PrefabChildBlueprint
 import com.github.bratek20.utils.directory.api.File
 import com.github.bratek20.utils.directory.api.FileContent
 import com.github.bratek20.utils.directory.api.FileName
@@ -12,25 +16,29 @@ class PrefabBlueprintsGenerator: PatternGenerator() {
     }
 
     override fun generateFiles(): List<File> {
+        val blueprint = PrefabBlueprint.create(
+            blueprintType = BlueprintType.UiElement,
+            name = "OtherClass",
+            viewType = "OtherModule.View.OtherClassView",
+            creationOrder = 1,
+            children = listOf(
+                PrefabChildBlueprint.create(
+                    name = "id",
+                    viewType = "B20.Frontend.Elements.View.LabelView"
+                ),
+                PrefabChildBlueprint.create(
+                    name = "amount",
+                    viewType = "B20.Frontend.Elements.View.LabelView"
+                )
+            )
+        )
+
+        val serializer = SerializationFactory.createSerializer()
+        val serialized = serializer.serialize(blueprint)
         return listOf(
             File.create(
                 name = FileName("OtherClass.json"),
-                content = FileContent.fromString("""
-                    {
-                      "name": "OtherClass",
-                      "viewType": "OtherModule.View.OtherClassView",
-                      "children": [
-                        {
-                          "name": "id",
-                          "viewType": "B20.Frontend.Elements.View.LabelView"
-                        },
-                        {
-                          "name": "amount",
-                          "viewType": "B20.Frontend.Elements.View.LabelView"
-                        }
-                      ]
-                    }
-                """.trimIndent())
+                content = FileContent.fromString(serialized.getValue())
             )
         )
     }
