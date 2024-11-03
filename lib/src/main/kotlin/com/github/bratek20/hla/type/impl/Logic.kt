bb56d3356d15c83689fdd8ae7c8445c744f18f8a
Bartosz Rudzki (ROG FLOW) <bartosz.rudzki@tensquaregames.com>
2024-11-03 21:21:46 +0100

    step

--- lib/src/main/kotlin/com/github/bratek20/hla/type/impl/Logic.kt
+++ lib/src/main/kotlin/com/github/bratek20/hla/type/impl/Logic.kt
@@ -28,6 +28,46 @@ class TypeApiLogic: TypeApi {
                 )
             )
         }
+        if (type.getName() == "SomeClass3View") {
+            return listOf(
+                HlaType.create(
+                    "SomeClass2View",
+                    type.getPath()
+                ),
+                HlaType.create(
+                    "SomeEnumSwitchView",
+                    type.getPath()
+                ),
+                HlaType.create(
+                    "SomeClass2GroupView",
+                    type.getPath()
+                )
+            )
+        }
+        if (type.getName() == "SomeClass6View") {
+            return listOf(
+                HlaType.create(
+                    "OptionalSomeClassView",
+                    type.getPath()
+                ),
+                HlaType.create(
+                    "SomeClass2GroupView",
+                    type.getPath()
+                )
+            )
+        }
+        if (type.getName() == "SomeWindowView") {
+            return listOf(
+                HlaType.create(
+                    "SomeClassView",
+                    type.getPath()
+                ),
+                HlaType.create(
+                    "SomeClassGroupView",
+                    type.getPath()
+                )
+            )
+        }
 
         return emptyList()
     }
