5ec7d0739e16ed8f498bc780199598bb007a527e
Bartosz Rudzki (ROG FLOW) <bartosz.rudzki@tensquaregames.com>
2024-11-15 18:30:51 +0100

    green

--- lib/src/main/kotlin/com/github/bratek20/hla/queries/api/ModuleGroupQueries.kt
+++ lib/src/main/kotlin/com/github/bratek20/hla/queries/api/ModuleGroupQueries.kt
@@ -98,11 +98,9 @@ abstract class ApiPatternPopulator {
     }
 }
 
-class SimpleValueObjectsPopulator(
+abstract class SimpleStructurePopulator(
     private val defs: List<SimpleStructureDefinition>
 ): ApiPatternPopulator() {
-    override fun getPatternName() = PatternName.ValueObjects
-
     override fun getTypeNames(): List<String> {
         return defs.map { it.getName() }
     }
@@ -120,11 +118,21 @@ class SimpleValueObjectsPopulator(
     }
 }
 
-class ComplexValueObjectsPopulator(
+class SimpleValueObjectsPopulator(
+    defs: List<SimpleStructureDefinition>
+): SimpleStructurePopulator(defs) {
+    override fun getPatternName() = PatternName.ValueObjects
+}
+
+class SimpleCustomTypesPopulator(
+    defs: List<SimpleStructureDefinition>
+): SimpleStructurePopulator(defs) {
+    override fun getPatternName() = PatternName.CustomTypes
+}
+
+abstract class ComplexStructuresPopulator(
     private val defs: List<ComplexStructureDefinition>
 ): ApiPatternPopulator() {
-    override fun getPatternName() = PatternName.ValueObjects
-
     override fun getTypeNames(): List<String> {
         return defs.map { it.getName() }
     }
@@ -136,6 +144,46 @@ class ComplexValueObjectsPopulator(
                 fields = def.getFields().map { it.asClassField(world) }
             ))
         }
+    }
+}
+
+class ComplexValueObjectsPopulator(
+    defs: List<ComplexStructureDefinition>
+): ComplexStructuresPopulator(defs) {
+    override fun getPatternName() = PatternName.ValueObjects
+}
+
+class ComplexCustomTypesPopulator(
+    defs: List<ComplexStructureDefinition>
+): ComplexStructuresPopulator(defs) {
+    override fun getPatternName() = PatternName.CustomTypes
+}
+
+class DataClassesPopulator(
+    defs: List<ComplexStructureDefinition>
+): ComplexStructuresPopulator(defs) {
+    override fun getPatternName() = PatternName.DataClasses
+}
+
+class EnumsPopulator(
+    private val defs: List<EnumDefinition>
+): ApiPatternPopulator() {
+    override fun getPatternName() = PatternName.Enums
+
+    override fun getTypeNames(): List<String> {
+        return defs.map { it.getName() }
+    }
+}
+
+//TODO-FIX current abstraction is not suited for external types i.e they do not have pattern name
+class ExternalTypesPopulator(
+    private val defs: List<String>
+): ApiPatternPopulator() {
+    //TODO-FIX
+    override fun getPatternName() = PatternName.ValueObjects
+
+    override fun getTypeNames(): List<String> {
+        return defs
     }
 }
 
@@ -159,7 +207,16 @@ class ApiTypesPopulator(
     private fun createPatternPopulators(module: ModuleDefinition): List<ApiPatternPopulator> {
         val populators = listOf(
             SimpleValueObjectsPopulator(module.getSimpleValueObjects()),
-            ComplexValueObjectsPopulator(module.getComplexValueObjects())
+            ComplexValueObjectsPopulator(module.getComplexValueObjects()),
+
+            SimpleCustomTypesPopulator(module.getSimpleCustomTypes()),
+            ComplexCustomTypesPopulator(module.getComplexCustomTypes()),
+
+            DataClassesPopulator(module.getDataClasses()),
+
+            EnumsPopulator(module.getEnums()),
+
+            ExternalTypesPopulator(module.getExternalTypes())
         )
 
         populators.forEach { populator ->
