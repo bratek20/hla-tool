bc4ae2cbd6c7e369285a5316615a44185c8a557b
Bartosz Rudzki (Legion) <bartosz.rudzki@tensquaregames.com>
2025-03-17 13:48:50 +0100

    st

--- example/typescript/Tests/SomeModule/Fixtures/Mocks.ts
+++ example/typescript/Tests/SomeModule/Fixtures/Mocks.ts
@@ -47,3 +47,74 @@ class SomeModuleDebugHandlersMock implements SomeModuleDebugHandlers {
     someDebugHandler2(): void {
     }
 }
+
+namespace SomeModule.Mocks {
+    export function createSomeEmptyInterfaceMock(): SomeEmptyInterfaceMock {
+        return new SomeEmptyInterfaceMock()
+    }
+
+    export function setupSomeEmptyInterface(): SomeEmptyInterfaceMock {
+        const mock = SomeModule.Mocks.createSomeEmptyInterfaceMock()
+        return mock
+    }
+
+    export function createSomeInterfaceMock(): SomeInterfaceMock {
+        return new SomeInterfaceMock()
+    }
+
+    export function setupSomeInterface(): SomeInterfaceMock {
+        const mock = SomeModule.Mocks.createSomeInterfaceMock()
+        SomeModule.Api.someEmptyMethod = CreateMock(SomeModule.Api.someEmptyMethod, () => { mock.someEmptyMethod() })
+        SomeModule.Api.someCommand = CreateMock(SomeModule.Api.someCommand, () => { mock.someCommand() })
+        SomeModule.Api.someQuery = CreateMock(SomeModule.Api.someQuery, () => { mock.someQuery() })
+        SomeModule.Api.optMethod = CreateMock(SomeModule.Api.optMethod, () => { mock.optMethod() })
+        SomeModule.Api.methodWithListOfSimpleVO = CreateMock(SomeModule.Api.methodWithListOfSimpleVO, () => { mock.methodWithListOfSimpleVO() })
+        SomeModule.Api.methodWithAny = CreateMock(SomeModule.Api.methodWithAny, () => { mock.methodWithAny() })
+        SomeModule.Api.methodReturningOptSimpleVo = CreateMock(SomeModule.Api.methodReturningOptSimpleVo, () => { mock.methodReturningOptSimpleVo() })
+        return mock
+    }
+
+    export function createSomeInterface2Mock(): SomeInterface2Mock {
+        return new SomeInterface2Mock()
+    }
+
+    export function setupSomeInterface2(): SomeInterface2Mock {
+        const mock = SomeModule.Mocks.createSomeInterface2Mock()
+        SomeModule.Api.referenceOtherClass = CreateMock(SomeModule.Api.referenceOtherClass, () => { mock.referenceOtherClass() })
+        SomeModule.Api.referenceLegacyType = CreateMock(SomeModule.Api.referenceLegacyType, () => { mock.referenceLegacyType() })
+        return mock
+    }
+
+    export function createSomeInterface3Mock(): SomeInterface3Mock {
+        return new SomeInterface3Mock()
+    }
+
+    export function setupSomeInterface3(): SomeInterface3Mock {
+        const mock = SomeModule.Mocks.createSomeInterface3Mock()
+        SomeModule.Api.referenceInterface = CreateMock(SomeModule.Api.referenceInterface, () => { mock.referenceInterface() })
+        SomeModule.Api.referenceOtherInterface = CreateMock(SomeModule.Api.referenceOtherInterface, () => { mock.referenceOtherInterface() })
+        return mock
+    }
+
+    export function createSomeModuleHandlersMock(): SomeModuleHandlersMock {
+        return new SomeModuleHandlersMock()
+    }
+
+    export function setupSomeModuleHandlers(): SomeModuleHandlersMock {
+        const mock = SomeModule.Mocks.createSomeModuleHandlersMock()
+        SomeModule.Api.someHandler = CreateMock(SomeModule.Api.someHandler, () => { mock.someHandler() })
+        SomeModule.Api.someHandler2 = CreateMock(SomeModule.Api.someHandler2, () => { mock.someHandler2() })
+        return mock
+    }
+
+    export function createSomeModuleDebugHandlersMock(): SomeModuleDebugHandlersMock {
+        return new SomeModuleDebugHandlersMock()
+    }
+
+    export function setupSomeModuleDebugHandlers(): SomeModuleDebugHandlersMock {
+        const mock = SomeModule.Mocks.createSomeModuleDebugHandlersMock()
+        SomeModule.Api.someDebugHandler = CreateMock(SomeModule.Api.someDebugHandler, () => { mock.someDebugHandler() })
+        SomeModule.Api.someDebugHandler2 = CreateMock(SomeModule.Api.someDebugHandler2, () => { mock.someDebugHandler2() })
+        return mock
+    }
+}
