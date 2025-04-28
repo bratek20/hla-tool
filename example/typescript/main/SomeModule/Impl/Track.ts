4aad1368fe10fed4a44b2d3628d7b211afa660a6
Andrea Alcaras <a.alcaras@rortos.com>
2025-04-28 11:57:19 +0200

    expected changes for optional TrackingDimension

--- example/typescript/main/SomeModule/Impl/Track.ts
+++ example/typescript/main/SomeModule/Impl/Track.ts
@@ -35,11 +35,11 @@ namespace SomeModule.Impl {
     export class SomeTrackingEvent extends TrackingEvent {
         constructor(
             some_dimension_id: SomeDimension,
-            some_dimension_id_optional: TrackingDimension
+            some_dimension_id_optional: Optional<TrackingDimension>
         ) {
             super()
             this.some_dimension_id = some_dimension_id
-            this.some_dimension_id_optional = some_dimension_id_optional
+            this.some_dimension_id_optional = some_dimension_id_optional.orElse(null)
         }
         private readonly some_dimension_id: SomeDimension
         private readonly some_dimension_id_optional: TrackingDimension
