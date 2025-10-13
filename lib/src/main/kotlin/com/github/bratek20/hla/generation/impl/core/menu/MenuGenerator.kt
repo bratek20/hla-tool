d65259ed5e8baf6fb0ee6ac2409ce2b378c1ded7
pawel.mierzwinski <pawel.mierzwinski@tensquaregames.com>
2025-10-13 12:27:34 +0200

    code refactor

--- lib/src/main/kotlin/com/github/bratek20/hla/generation/impl/core/menu/MenuGenerator.kt
+++ lib/src/main/kotlin/com/github/bratek20/hla/generation/impl/core/menu/MenuGenerator.kt
@@ -1,14 +1,8 @@
 package com.github.bratek20.hla.generation.impl.core.menu
 
-import com.github.bratek20.hla.definitions.api.KeyDefinition
-import com.github.bratek20.hla.facade.api.ModuleLanguage
 import com.github.bratek20.hla.generation.api.SubmoduleName
 import com.github.bratek20.hla.generation.impl.core.PatternGenerator
 import com.github.bratek20.hla.generation.impl.core.SubmoduleGenerator
-import com.github.bratek20.hla.generation.impl.core.examples.patterns.ExampleKeyDefinitionLogic
-import com.github.bratek20.hla.generation.impl.core.menu.patterns.MenuPatternGenerator
-import com.github.bratek20.utils.directory.api.Directory
-import com.github.bratek20.utils.directory.api.DirectoryName
 
 class MenuGenerator: SubmoduleGenerator() {
 
@@ -23,7 +17,7 @@ class MenuGenerator: SubmoduleGenerator() {
 
     override fun getPatternGenerators(): List<PatternGenerator> {
         return listOf(
-                MenuPatternGenerator()
+                MenuPattern()
         )
     }
 
