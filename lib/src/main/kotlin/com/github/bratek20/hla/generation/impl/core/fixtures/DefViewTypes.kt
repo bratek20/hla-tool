4fe48a4f5be0328d49c194016543ff5ddf5bf1ee
Andrea Alcaras <a.alcaras@rortos.com>
2025-11-18 15:33:37 +0100

    fixing builders

--- lib/src/main/kotlin/com/github/bratek20/hla/generation/impl/core/fixtures/DefViewTypes.kt
+++ lib/src/main/kotlin/com/github/bratek20/hla/generation/impl/core/fixtures/DefViewTypes.kt
@@ -129,7 +129,9 @@ open class DefField(
     val api: ComplexStructureField,
     private val factory: DefTypeFactory
 ) {
-    val name = api.name
+    val kotlinPrivateWords = listOf("as")
+
+    val name = if(api.type.languageTypes is KotlinTypes && kotlinPrivateWords.contains(api.name)) "`${api.name}`"  else api.name
 
     val type by lazy {
         factory.create(api.type)
